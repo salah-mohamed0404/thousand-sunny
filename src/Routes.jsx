6d6201039997a5ef1acf5294c64cafec315aa58b
salahcst344 <salahcst344@gmail.com>
2023-01-17 18:42:09 +0200

    Basic routes structure

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+import ProductDetails from "./containers/ProductDetails";
+import Products from "./containers/Products";
+import UserProfile from "./containers/UserProfile";
 
 export default function Links() {
   return (
     <main>
       <Routes>
-        <Route path="/" element={<div>Routes</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </main>
   );
