e546d76378d142c4f2c981a06d5fcdf65f145230
salahcst344 <salahcst344@gmail.com>
2023-01-17 16:55:58 +0200

    Build main navigation

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { AppBar, Toolbar, Stack } from "@mui/material";
+import LiveSearch from "./LiveSearch";
+import logo from "../assets/thousand-sunny-logo.png";
+import NavButtons from "./NavButtons";
+
+const MainNav = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Stack
+          direction="row"
+          spacing={1}
+          alignItems="center"
+          justifyContent="space-between"
+          sx={{ width: "100%" }}
+        >
+          <Link to="/">
+            <img src={logo} alt="website logo" style={{ width: 64 }} />
+          </Link>
+
+          <LiveSearch />
+
+          <NavButtons />
+        </Stack>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default MainNav;
