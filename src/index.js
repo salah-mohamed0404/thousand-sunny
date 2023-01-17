99cb43d5230a939d8d0b15e81d5c426a96030838
salahcst344 <salahcst344@gmail.com>
2023-01-17 16:53:45 +0200

    Add ThemeContextProvider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeContextProvider } from "./store/theme-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </ThemeContextProvider>
   </React.StrictMode>
 );
