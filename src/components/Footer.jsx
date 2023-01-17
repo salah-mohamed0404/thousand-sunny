e2a2616e915320f092f24cab4b84d0475b22eda2
salahcst344 <salahcst344@gmail.com>
2023-01-17 21:10:19 +0200

    Inhance not found route

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
+import { Box, Grid } from "@mui/material";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box component="footer" borderTop="">
+      <Grid container>
+        <Grid item></Grid>
+        <Grid item></Grid>
+        <Grid item component="nav"></Grid>
+        <Grid item component="nav"></Grid>
+        <Grid item component="nav"></Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Footer;
