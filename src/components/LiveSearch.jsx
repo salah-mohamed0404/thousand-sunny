d2cdd5dfa8e4695810a4fdff323d3b50e0dd630f
salahcst344 <salahcst344@gmail.com>
2023-01-17 19:30:55 +0200

    Some inhancment and solve light mode color

--- src/components/LiveSearch.jsx
+++ src/components/LiveSearch.jsx
@@ -1,9 +1,16 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import CircularProgress from "@mui/material/CircularProgress";
 import axios from "axios";
-import { Avatar, InputAdornment, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  createTheme,
+  InputAdornment,
+  Stack,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import { Search } from "@mui/icons-material";
 
 function LiveSearch() {
@@ -11,11 +18,21 @@ function LiveSearch() {
   const [options, setOptions] = useState([]);
   const [searchRes, setSearchRes] = useState(null);
   const [loading, setLoading] = useState(false);
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: "dark",
+          primary: { main: "#fff" },
+        },
+      }),
+    []
+  );
 
   const fetchProducts = useCallback(async (searchTerm = "") => {
     setLoading(true);
     const res = await axios.get(
-      `https://dummyjson.com/products/search?q=${searchTerm}`
+      `https://dummyjson.com/products/search?q=${searchTerm}&limit=5`
     );
     setOptions(res.data.products);
     setLoading(false);
@@ -37,67 +54,69 @@ function LiveSearch() {
 
   // Rendering our parameters on the DOM
   return (
-    <Autocomplete
-      id="live-search"
-      options={options}
-      value={searchRes}
-      loading={loading}
-      open={open}
-      sx={{ width: 300 }}
-      freeSolo
-      autoComplete
-      openOnFocus
-      disableClearable
-      disableListWrap
-      onOpen={() => {
-        setOpen(true);
-      }}
-      onClose={() => {
-        setOpen(false);
-      }}
-      onChange={(e, newValue) => handleSearchRes(newValue)}
-      onInputChange={(e, newInputValue, reason) => {
-        if (reason === "input") fetchProducts(newInputValue);
-      }}
-      isOptionEqualToValue={(option, value) => option.title === value.title}
-      getOptionLabel={(option) => (option.title ? option.title : option)}
-      // clearIcon={<clearIcon size="small" color="red" />}
-      renderOption={(props, option) => (
-        <Stack
-          component="li"
-          direction="row"
-          spacing={2}
-          flexWrap="nowrap"
-          {...props}
-        >
-          <Avatar variant="circular" src={option.images[0]} />
-          <Typography flexGrow={1}>{option.title}</Typography>
-        </Stack>
-      )}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          variant="standard"
-          label="Search"
-          InputProps={{
-            ...params.InputProps,
-            startAdornment: (
-              <InputAdornment position="start">
-                <Search />
-              </InputAdornment>
-            ),
-            endAdornment: (
-              <>
-                {loading ? (
-                  <CircularProgress color="inherit" size={20} />
-                ) : null}
-              </>
-            ),
-          }}
-        />
-      )}
-      noOptionsText="NO AVAILABLE PRODUCTS😥"
-    />
+    <ThemeProvider theme={theme}>
+      <Autocomplete
+        id="live-search"
+        options={options}
+        value={searchRes}
+        loading={loading}
+        open={open}
+        sx={{ width: 300 }}
+        freeSolo
+        autoComplete
+        openOnFocus
+        disableClearable
+        disableListWrap
+        onOpen={() => {
+          setOpen(true);
+        }}
+        onClose={() => {
+          setOpen(false);
+        }}
+        onChange={(e, newValue) => handleSearchRes(newValue)}
+        onInputChange={(e, newInputValue, reason) => {
+          if (reason === "input") fetchProducts(newInputValue);
+        }}
+        isOptionEqualToValue={(option, value) => option.title === value.title}
+        getOptionLabel={(option) => (option.title ? option.title : option)}
+        // clearIcon={<clearIcon size="small" color="red" />}
+        renderOption={(props, option) => (
+          <Stack
+            component="li"
+            direction="row"
+            spacing={2}
+            flexWrap="nowrap"
+            {...props}
+          >
+            <Avatar variant="circular" src={option.images[0]} />
+            <Typography flexGrow={1}>{option.title}</Typography>
+          </Stack>
+        )}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            variant="standard"
+            label="Search"
+            InputProps={{
+              ...params.InputProps,
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Search />
+                </InputAdornment>
+              ),
+              endAdornment: (
+                <>
+                  {loading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                </>
+              ),
+            }}
+          />
+        )}
+        noOptionsText="NO AVAILABLE PRODUCTS😥"
+      />
+    </ThemeProvider>
   );
 }
 
