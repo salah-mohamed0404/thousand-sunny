073b2ac05261ff055ce5f0c3a68645cb764f1ed5
salahcst344 <salahcst344@gmail.com>
2023-01-17 16:54:59 +0200

    Basic structure for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { useEffect, useState, lazy, Suspense } from "react";
+const MainNav = lazy(() => import("./MainNav"));
+const MobileNav = lazy(() => import("./MobileNav"));
+
 const Header = () => {
-  return <div>Header</div>;
+  const [isDesktop, setDesktop] = useState(false);
+
+  useEffect(() => {
+    const updateMedia = () => {
+      if (window.innerWidth > 900) {
+        setDesktop(true);
+      } else {
+        setDesktop(false);
+      }
+    };
+    updateMedia();
+
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, []);
+
+  return <Suspense>{isDesktop ? <MainNav /> : <MobileNav />}</Suspense>;
 };
 
 export default Header;
