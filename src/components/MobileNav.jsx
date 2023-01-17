e87154181bcf39c334341980f8e4f8c2f252dcbc
salahcst344 <salahcst344@gmail.com>
2023-01-17 16:56:18 +0200

    Init mobile navigtion

---
+++ src/components/MobileNav.jsx
@@ -0,0 +1,12 @@
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
+import React from "react";
+
+const MobileNav = () => {
+  return (
+    <BottomNavigation>
+      <BottomNavigationAction />
+    </BottomNavigation>
+  );
+};
+
+export default MobileNav;
