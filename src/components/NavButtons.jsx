28be903da6de6f8b207fe05bd6460ce18f431751
salahcst344 <salahcst344@gmail.com>
2023-01-17 16:56:57 +0200

    Build NavButtons componant

---
+++ src/components/NavButtons.jsx
@@ -0,0 +1,51 @@
+import { useContext } from "react";
+import { Stack, Avatar, Button, IconButton } from "@mui/material";
+import {
+  LightMode,
+  PersonIcon,
+  FavoriteBorder,
+  LocalMall,
+  DarkMode,
+} from "@mui/icons-material";
+import ThemeContext from "../store/theme-context";
+
+const NavButtons = () => {
+  const { mode, toggleMode } = useContext(ThemeContext);
+
+  // const fetchCategories = useCallback(
+  //   async () => await axios.get("https://dummyjson.com/products/categories"),
+  //   []
+  // );
+
+  // useEffect(() => {
+  //   fetchCategories().then((res) => setCategories(res.data));
+  // }, [fetchCategories]);
+
+  return (
+    <Stack direction="row" spacing={4}>
+      {/* <Link to="/user-profile">
+        <Avatar>
+          <PersonIcon />
+        </Avatar>
+      </Link> */}
+      <Button variant="text" color="inherit">
+        Sign in
+      </Button>
+      <Button variant="text" endIcon={<FavoriteBorder />} color="inherit">
+        WishList
+      </Button>
+      <Button variant="text" endIcon={<LocalMall />} color="inherit">
+        Cart
+      </Button>
+      <IconButton onClick={toggleMode}>
+        {mode === "dark" ? (
+          <LightMode color="inherit" />
+        ) : (
+          <DarkMode color="inherit" />
+        )}
+      </IconButton>
+    </Stack>
+  );
+};
+
+export default NavButtons;
