22aeb5e5081e0dccaa31e2d9f40e18e7385c73d9
salahcst344 <salahcst344@gmail.com>
2023-01-18 16:15:48 +0200

    Inhance not found route

--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -1,21 +1,30 @@
-import { Button, Stack, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import NotFoundImg from "../assets/not-found.svg";
 const NotFound = () => {
   return (
-    <Stack alignItems="center" justifyContent="center" py={5} px={30}>
-      <img src={NotFoundImg} alt="Page not found" height={300} />
-      <Typography variant="h5" mt={10} mb={5} textAlign="center">
-        The page you are looking for could not be found. The link to this
-        address may be outdated or we may have moved the since you last
-        bookmarked it.Sorry😥
-      </Typography>
-      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-        <Button variant="contained" size="large">
-          Go home
-        </Button>
-      </Link>
-    </Stack>
+    <Container maxWidth="md">
+      <Stack alignItems="center" justifyContent="center" py={6}>
+        <img src={NotFoundImg} alt="Page not found" width="75%" />
+
+        <Typography
+          variant="h5"
+          mt={10}
+          mb={5}
+          textAlign="center"
+          maxWidth={1000}
+        >
+          The page you are looking for could not be found. The link to this
+          address may be outdated or we may have moved the since you last
+          bookmarked it.Sorry😥
+        </Typography>
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Button variant="contained" size="large">
+            Back to home
+          </Button>
+        </Link>
+      </Stack>
+    </Container>
   );
 };
 
