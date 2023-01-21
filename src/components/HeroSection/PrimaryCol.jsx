2ffa37da2bf8cd257fc9b6713ab97485800bc6ca
salahcst344 <salahcst344@gmail.com>
2023-01-21 15:59:21 +0200

    Add hero column wrapper to Primary column

--- src/components/HeroSection/PrimaryCol.jsx
+++ src/components/HeroSection/PrimaryCol.jsx
@@ -1,25 +1,11 @@
-import { alpha, Button, Grid, Paper, Stack, Typography } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import { Button, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import laptop from "../../assets/laptop.jpg";
+import ColWrapper from "./ColWrapper";
 
 const PrimaryCol = () => {
   return (
-    <Grid
-      item
-      md={6}
-      p={5}
-      borderRadius={1}
-      sx={{
-        backgroundImage: `linear-gradient(to bottom right, ${alpha(
-          grey[700],
-          0.5
-        )} , transparent), url(${laptop})`,
-        backgroundSize: "cover",
-        backgroundRepeat: "no-repeat",
-      }}
-      component={Paper}
-    >
+    <ColWrapper bgImg={laptop}>
       <Stack width="50%" spacing={2}>
         <Typography
           variant="subtitle1"
@@ -38,7 +24,7 @@ const PrimaryCol = () => {
           <Button variant="contained">Shop Now</Button>
         </Link>
       </Stack>
-    </Grid>
+    </ColWrapper>
   );
 };
 
