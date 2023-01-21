4d38af5762b4b33d3f2da1c1c9d08e8b5da88af2
salahcst344 <salahcst344@gmail.com>
2023-01-21 15:48:01 +0200

    Build Hero Section

---
+++ src/components/HeroSection/index.jsx
@@ -0,0 +1,16 @@
+import HeroWrapper from "./HeroWrapper";
+import PrimaryCol from "./PrimaryCol";
+import SecodaryCol from "./SecodaryCol";
+import TertiaryCol from "./TertiaryCol";
+
+const HeroSection = () => {
+  return (
+    <HeroWrapper>
+      <PrimaryCol />
+      <SecodaryCol />
+      <TertiaryCol />
+    </HeroWrapper>
+  );
+};
+
+export default HeroSection;
