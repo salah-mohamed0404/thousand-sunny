849a65f705c144f12b68cfabf921ed1e883af3cf
salahcst344 <salahcst344@gmail.com>
2023-01-22 15:57:39 +0200

    Add features section to home route

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,12 @@
 import { Stack } from "@mui/material";
+import Features from "../components/Features";
 import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   return (
     <Stack>
       <HeroSection />
+      <Features />
     </Stack>
   );
 };
