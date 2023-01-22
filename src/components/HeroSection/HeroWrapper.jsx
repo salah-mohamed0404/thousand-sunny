5bd5e64a4e82c71e1c4cac14d087dece1797dc9c
salahcst344 <salahcst344@gmail.com>
2023-01-22 12:10:15 +0200

    Fix issue in hero wrapper with light mode

--- src/components/HeroSection/HeroWrapper.jsx
+++ src/components/HeroSection/HeroWrapper.jsx
@@ -1,16 +1,20 @@
+import { useContext } from "react";
 import { alpha, Box, Container, Grid } from "@mui/material";
 import { grey } from "@mui/material/colors";
 import tech from "../../assets/tech-wallpaper.jpg";
+import ThemeContext from "../../store/theme-context";
 
 const HeroWrapper = ({ children }) => {
+  const { mode } = useContext(ThemeContext);
+
   return (
     <Box
       py={7}
       sx={{
         backgroundImage: `linear-gradient(to bottom right, ${alpha(
           grey[500],
-          0.3
-        )} , ${alpha(grey[400], 0.3)}), url(${tech})`,
+          mode === "dark" ? 0.4 : 0.1
+        )} , ${alpha(grey[400], mode === "dark" ? 0.4 : 0.1)}), url(${tech})`,
         backgroundSize: "cover",
       }}
       component="section"
