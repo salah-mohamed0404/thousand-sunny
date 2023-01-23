7e056dc4e539ab598d5e3381c10d238294717422
salahcst344 <salahcst344@gmail.com>
2023-01-23 20:02:55 +0200

    Build latest products panal

---
+++ src/components/HomeProducts/LatestProducts.jsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import HomeProductsLoading from "./HomeProductsLoading";
+import ProductsList from "../ProductList";
+
+const LatestProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://dummyjson.com/products?limit=12&select=id,title,rating,price,discountPercentage"
+      );
+      setProducts(res.data.products);
+      setLoading(false);
+    };
+    fetchProducts();
+  }, []);
+
+  return !loading ? (
+    <ProductsList products={products} breakpoints={{ xs: 3.75 }} />
+  ) : (
+    <HomeProductsLoading />
+  );
+};
+
+export default LatestProducts;
