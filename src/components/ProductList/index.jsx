b69f7780c7aad82e2b02d11c9e2c8bbd81f35988
salahcst344 <salahcst344@gmail.com>
2023-01-23 20:01:16 +0200

    Build product list componant

---
+++ src/components/ProductList/index.jsx
@@ -0,0 +1,33 @@
+import { CardMedia, Grid } from "@mui/material";
+import placeholderImg from "../../assets/placeholder.jpg";
+import ItemWapper from "./ItemWapper";
+import ProductActions from "./ProductActions";
+import ProductContent from "./ProductContent";
+
+const ProductsList = ({ col = 12, breakpoints, products = [] }) => {
+  return (
+    <Grid container columns={col} columnGap={2} rowGap={4}>
+      {products.map((product) => (
+        <ItemWapper key={product.id} breakpoints={breakpoints}>
+          <CardMedia
+            component="img"
+            height={200}
+            image={placeholderImg}
+            alt={product.title}
+          />
+
+          <ProductContent
+            title={product.title}
+            price={product.price}
+            discount={product.discountPercentage}
+            rating={product.rating}
+          />
+
+          <ProductActions productId={product.id} />
+        </ItemWapper>
+      ))}
+    </Grid>
+  );
+};
+
+export default ProductsList;
