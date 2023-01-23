8b5ea7312d3eef4385f2c4454da3ef1ade51a230
salahcst344 <salahcst344@gmail.com>
2023-01-23 20:02:35 +0200

    Build featured product panal

---
+++ src/components/HomeProducts/FeaturedProducts.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import HomeProductsLoading from "./HomeProductsLoading";
+import ProductsList from "../ProductList";
+
+const FeaturedProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      setLoading(true);
+      const res = await axios.get(
+        "https://dummyjson.com/products?limit=6&select=id,title,rating,price,discountPercentage"
+      );
+      setProducts(res.data.products);
+      setLoading(false);
+    };
+    fetchProducts();
+  }, []);
+
+  return !loading ? (
+    <ProductsList products={products} breakpoints={{ xs: 3.75 }} />
+  ) : (
+    <HomeProductsLoading />
+  );
+};
+
+export default FeaturedProducts;
