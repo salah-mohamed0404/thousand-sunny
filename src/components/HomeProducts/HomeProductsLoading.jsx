80a0cf1056b5cd244c0ef834127f9f3a77515461
salahcst344 <salahcst344@gmail.com>
2023-01-23 20:03:17 +0200

    Build home product loading

---
+++ src/components/HomeProducts/HomeProductsLoading.jsx
@@ -0,0 +1,7 @@
+import Loading from "../Loading";
+
+const HomeProductsLoading = () => {
+  return <Loading containerProps={{ my: 10 }} progressProps={{ size: 80 }} />;
+};
+
+export default HomeProductsLoading;
