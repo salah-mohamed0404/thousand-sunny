fa44ecd139dd440450817e04cf788395eadbde66
salahcst344 <salahcst344@gmail.com>
2023-01-26 13:47:46 +0200

    Build products route

--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { useCallback, useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import ProductsSection from "../components/ProductsSection";
+import axios from "axios";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProducts = useCallback(async (skip = 0) => {
+    setLoading(true);
+
+    const url = `https://dummyjson.com/products?limit=30&skip=${skip}`;
+    const res = await axios.get(url);
+
+    setProducts(res.data.products);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  const handleProducts = (products) => {
+    setProducts(products);
+  };
+
+  return (
+    <Box>
+      <Typography
+        variant="h2"
+        color="primary.main"
+        textAlign="center"
+        p={2}
+        gutterBottom
+      >
+        In your service
+      </Typography>
+
+      <ProductsSection
+        loading={loading}
+        products={products}
+        handleProducts={handleProducts}
+        fetchProducts={fetchProducts}
+      />
+    </Box>
+  );
 };
 
 export default Products;
