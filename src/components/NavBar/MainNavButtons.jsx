305b54a974447edfffb14179c87295387494bd1d
salahcst344 <salahcst344@gmail.com>
2023-01-27 20:07:56 +0200

    Add user avatar and make login/logout work

--- src/components/NavBar/MainNavButtons.jsx
+++ src/components/NavBar/MainNavButtons.jsx
@@ -1,49 +1,61 @@
 import { useContext } from "react";
-// eslint-disable-next-line
 import { Stack, Avatar, Button, IconButton, Tooltip } from "@mui/material";
 import {
   LightMode,
-  // eslint-disable-next-line
-  PersonIcon,
+  Person,
   FavoriteBorder,
   LocalMall,
   DarkMode,
+  Login,
+  Logout,
 } from "@mui/icons-material";
 import ThemeContext from "../../store/theme-context";
 import LiveSearch from "../LiveSearch";
 import { Link } from "react-router-dom";
+import AuthContext from "../../store/Auth-context";
 
-const NavButtons = () => {
+const MainNavButtons = () => {
   const { mode, toggleMode } = useContext(ThemeContext);
-
-  // const fetchCategories = useCallback(
-  //   async () => await axios.get("https://dummyjson.com/products/categories"),
-  //   []
-  // );
-
-  // useEffect(() => {
-  //   fetchCategories().then((res) => setCategories(res.data));
-  // }, [fetchCategories]);
+  const { user, isAuthenticated, logout } = useContext(AuthContext);
 
   return (
     <Stack direction="row" spacing={4} alignItems="center">
       <LiveSearch />
-      {/* <Link to="/user-profile">
-        <Avatar>
-          <PersonIcon />
-        </Avatar>
-      </Link> */}
-      <Link to="/login">
-        <Button variant="text" color="inherit">
-          Log in
-        </Button>
-      </Link>
       <Button variant="text" endIcon={<FavoriteBorder />} color="inherit">
         WishList
       </Button>
       <Button variant="text" endIcon={<LocalMall />} color="inherit">
         Cart
       </Button>
+      {!isAuthenticated ? (
+        <Link to="/login">
+          <Button variant="text" color="inherit" endIcon={<Login />}>
+            Log in
+          </Button>
+        </Link>
+      ) : (
+        <Button
+          variant="text"
+          color="inherit"
+          endIcon={<Logout />}
+          onClick={logout}
+        >
+          log out
+        </Button>
+      )}
+      {isAuthenticated && (
+        <Tooltip title="User profile">
+          <Link to="/user-profile">
+            <Avatar>
+              {user?.image ? (
+                <img src={user.image} alt="user " width="100%" />
+              ) : (
+                <Person />
+              )}
+            </Avatar>
+          </Link>
+        </Tooltip>
+      )}
       <Tooltip title={mode === "dark" ? "Light mode" : "Dark mode"}>
         <IconButton onClick={toggleMode} color="inherit">
           {mode === "dark" ? <LightMode /> : <DarkMode />}
@@ -53,4 +65,4 @@ const NavButtons = () => {
   );
 };
 
-export default NavButtons;
+export default MainNavButtons;
