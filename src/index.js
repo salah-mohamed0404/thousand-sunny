6fd6a6212deaa27b182dca6f9339821e86e61078
salahcst344 <salahcst344@gmail.com>
2023-01-27 18:09:22 +0200

    Add auth context provider to the main app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeContextProvider } from "./store/theme-context";
+import { AuthContextProvider } from "./store/Auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeContextProvider>
-      <Router>
-        <App />
-      </Router>
-    </ThemeContextProvider>
+    <AuthContextProvider>
+      <ThemeContextProvider>
+        <Router>
+          <App />
+        </Router>
+      </ThemeContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
