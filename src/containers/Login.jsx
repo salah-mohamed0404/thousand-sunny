d3886e15a65ec22e830f93f5c9e7a1105e7d7cbe
salahcst344 <salahcst344@gmail.com>
2023-01-27 18:12:04 +0200

    Build log in route

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Box, Container, Paper, Stack, Typography } from "@mui/material";
+import LoginForm from "../components/LoginForm";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Box mt={5}>
+      <Container>
+        <Paper
+          component={Stack}
+          p={5}
+          spacing={3}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="h3" textAlign="center" gutterBottom>
+            Log in to 1000Sunny
+          </Typography>
+
+          <LoginForm />
+        </Paper>
+      </Container>
+    </Box>
+  );
 };
 
 export default Login;
