09c6d95ce063504df27f31c92fb0222406cc18a9
salahcst344 <salahcst344@gmail.com>
2023-01-28 16:46:46 +0200

    Create sign up route

--- src/containers/SignUp.jsx
+++ src/containers/SignUp.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Box, Container, Paper, Stack, Typography } from "@mui/material";
+import SignUpForm from "../components/SignUpForm";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <Box mt={5}>
+      <Container>
+        <Paper
+          component={Stack}
+          p={5}
+          spacing={3}
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Typography variant="h4" textAlign="center" gutterBottom>
+            🤗Sign up to 1000Sunny🤗
+          </Typography>
+
+          <SignUpForm />
+        </Paper>
+      </Container>
+    </Box>
+  );
 };
 
 export default SignUp;
