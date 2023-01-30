acc0e2eed0caa686125c0eb5d403832870b3f4ce
salahcst344 <salahcst344@gmail.com>
2023-01-30 13:31:31 +0200

    Remove show details button

--- src/components/ProductList/ProductActions.jsx
+++ src/components/ProductList/ProductActions.jsx
@@ -1,34 +1,19 @@
-import {
-  FavoriteBorder,
-  LocalMallOutlined,
-  Visibility,
-} from "@mui/icons-material";
-import { Box, CardActions, IconButton, Tooltip } from "@mui/material";
-import React from "react";
-import { Link } from "react-router-dom";
+import { FavoriteBorder, LocalMallOutlined } from "@mui/icons-material";
+import { CardActions, IconButton, Tooltip } from "@mui/material";
 
-const ProductActions = ({ productId }) => {
+const ProductActions = () => {
   return (
-    <CardActions sx={{ flexGrow: 1 }}>
-      <Box mt="auto">
-        <Tooltip title="Show details">
-          <Link to={`/products/${productId}`}>
-            <IconButton>
-              <Visibility />
-            </IconButton>
-          </Link>
-        </Tooltip>
-        <Tooltip title="Add to cart">
-          <IconButton>
-            <LocalMallOutlined />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="Add to wishlist">
-          <IconButton>
-            <FavoriteBorder />
-          </IconButton>
-        </Tooltip>
-      </Box>
+    <CardActions sx={{ pl: 2 }}>
+      <Tooltip title="Add to cart">
+        <IconButton>
+          <LocalMallOutlined />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Add to wishlist">
+        <IconButton>
+          <FavoriteBorder />
+        </IconButton>
+      </Tooltip>
     </CardActions>
   );
 };
