ffa3101bd7760837bcd85f17c5d1ca553561e8d5
salahcst344 <salahcst344@gmail.com>
2023-01-30 17:15:19 +0200

    Make wishlist button link work

--- src/components/NavBar/WishlistButton.jsx
+++ src/components/NavBar/WishlistButton.jsx
@@ -1,11 +1,14 @@
 import { FavoriteBorder } from "@mui/icons-material";
 import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const WishlistButton = () => {
   return (
-    <Button variant="text" endIcon={<FavoriteBorder />} color="inherit">
-      WishList
-    </Button>
+    <Link to="/wishlist">
+      <Button variant="text" endIcon={<FavoriteBorder />} color="inherit">
+        WishList
+      </Button>
+    </Link>
   );
 };
 
