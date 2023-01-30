0c3b53a958d0d6102030cebd624410d7b614adf2
salahcst344 <salahcst344@gmail.com>
2023-01-30 13:32:26 +0200

    Fix styling issue

--- src/components/ProductList/ProductContent.jsx
+++ src/components/ProductList/ProductContent.jsx
@@ -1,41 +1,44 @@
-import { CardContent, Rating, Stack, Typography } from "@mui/material";
+import { Box, CardContent, Rating, Stack, Typography } from "@mui/material";
 
 const ProductContent = ({ title, price, discount, rating = 0 }) => {
   return (
-    <CardContent>
-      <Typography gutterBottom variant="h6">
-        {title}
-      </Typography>
-
-      <Stack direction="row" alignItems="center" spacing={1}>
-        <Rating
-          value={parseFloat(rating.toFixed(1))}
-          precision={0.1}
-          readOnly
-        />
-        <Typography variant="body2" color="text.secondary">
-          ({rating.toFixed(1)})
+    <CardContent component={Stack} justifyContent="space-between" flexGrow={1}>
+      <Box>
+        <Typography gutterBottom variant="h6">
+          {title}
         </Typography>
-      </Stack>
+      </Box>
 
-      <Stack
-        direction="row"
-        flexWrap="wrap"
-        columnGap={2}
-        mt={2}
-        color="primary.main"
-      >
-        <Typography
-          variant="h6"
-          component="div"
-          fontWeight={500}
-          sx={{ textDecoration: discount > 0 ? "line-through" : "none" }}
+      <Stack spacing={2} flexGrow={1} justifyContent="flex-end">
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Rating
+            value={parseFloat(rating.toFixed(1))}
+            precision={0.1}
+            readOnly
+          />
+          <Typography variant="body2" color="text.secondary">
+            ({rating.toFixed(1)})
+          </Typography>
+        </Stack>
+
+        <Stack
+          direction="row"
+          flexWrap="wrap"
+          columnGap={2}
+          color="primary.main"
         >
-          $ {price.toFixed(2)}
-        </Typography>
-        <Typography variant="h6" component="div" fontWeight={500}>
-          $ {(price * ((discount - 1) / 100)).toFixed(2)}
-        </Typography>
+          <Typography
+            variant="h6"
+            component="div"
+            fontWeight={500}
+            sx={{ textDecoration: discount > 0 ? "line-through" : "none" }}
+          >
+            $ {price.toFixed(2)}
+          </Typography>
+          <Typography variant="h6" component="div" fontWeight={500}>
+            $ {(price * ((discount - 1) / 100)).toFixed(2)}
+          </Typography>
+        </Stack>
       </Stack>
     </CardContent>
   );
