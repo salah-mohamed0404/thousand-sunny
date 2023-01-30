71f10bef2bfcf3728f0580be6396531de6e3b6a1
salahcst344 <salahcst344@gmail.com>
2023-01-30 13:29:14 +0200

    Add cart and wishlist context

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeContextProvider } from "./store/theme-context";
 import { AuthContextProvider } from "./store/Auth-context";
+import { CartContextProvider } from "./store/cart-context";
+import { WishlistContextProvider } from "./store/wishlist-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <ThemeContextProvider>
-        <Router>
-          <App />
-        </Router>
+        <WishlistContextProvider>
+          <CartContextProvider>
+            <Router>
+              <App />
+            </Router>
+          </CartContextProvider>
+        </WishlistContextProvider>
       </ThemeContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
