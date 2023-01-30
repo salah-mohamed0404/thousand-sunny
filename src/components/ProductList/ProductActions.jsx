b283ecafd10510c0a9b800b1d2123e6eb08ae91f
salahcst344 <salahcst344@gmail.com>
2023-01-30 14:48:47 +0200

    Make cart and wishlist product work

--- src/components/ProductList/ProductActions.jsx
+++ src/components/ProductList/ProductActions.jsx
@@ -1,17 +1,47 @@
-import { FavoriteBorder, LocalMallOutlined } from "@mui/icons-material";
+import { useMemo, useContext } from "react";
+import {
+  Favorite,
+  FavoriteBorder,
+  LocalMall,
+  LocalMallOutlined,
+} from "@mui/icons-material";
 import { CardActions, IconButton, Tooltip } from "@mui/material";
+import CartContext from "../../store/cart-context";
+import WishlistContext from "../../store/wishlist-context";
 
-const ProductActions = () => {
+const ProductActions = ({ productId }) => {
+  const { cartProductIds, addToCart, removeFromCart } = useContext(CartContext);
+  const { wishlistProductIds, addToWishlist, removeFromWishlist } =
+    useContext(WishlistContext);
+  const isInCart = useMemo(
+    () => cartProductIds.findIndex((id) => id === productId) !== -1,
+    [cartProductIds, productId]
+  );
+  const isInWishlist = useMemo(
+    () => wishlistProductIds.findIndex((id) => id === productId) !== -1,
+    [wishlistProductIds, productId]
+  );
+
+  const toggleCart = () => {
+    if (isInCart) removeFromCart(productId);
+    else addToCart(productId);
+  };
+
+  const toggleWishlist = () => {
+    if (isInWishlist) removeFromWishlist(productId);
+    else addToWishlist(productId);
+  };
+
   return (
     <CardActions sx={{ pl: 2 }}>
       <Tooltip title="Add to cart">
-        <IconButton>
-          <LocalMallOutlined />
+        <IconButton onClick={toggleCart}>
+          {!isInCart ? <LocalMallOutlined /> : <LocalMall />}
         </IconButton>
       </Tooltip>
       <Tooltip title="Add to wishlist">
-        <IconButton>
-          <FavoriteBorder />
+        <IconButton onClick={toggleWishlist}>
+          {!isInWishlist ? <FavoriteBorder /> : <Favorite />}
         </IconButton>
       </Tooltip>
     </CardActions>
