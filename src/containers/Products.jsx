75291215f9bad52e372240bbf9eea8400fa7b851
salahcst344 <salahcst344@gmail.com>
2023-02-01 18:06:43 +0200

    Inhance products route code

--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -1,30 +1,7 @@
-import { useCallback, useState, useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import ProductsSection from "../components/ProductsSection";
-import axios from "axios";
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const fetchProducts = useCallback(async (skip = 0) => {
-    setLoading(true);
-
-    const url = `https://dummyjson.com/products?limit=30&skip=${skip}`;
-    const res = await axios.get(url);
-
-    setProducts(res.data.products);
-    setLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchProducts();
-  }, [fetchProducts]);
-
-  const handleProducts = (products) => {
-    setProducts(products);
-  };
-
   return (
     <Box>
       <Typography
@@ -37,12 +14,7 @@ const Products = () => {
         In your service
       </Typography>
 
-      <ProductsSection
-        loading={loading}
-        products={products}
-        handleProducts={handleProducts}
-        fetchProducts={fetchProducts}
-      />
+      <ProductsSection />
     </Box>
   );
 };
