8125382063b1f04083c593028fa7889289765c64
salahcst344 <salahcst344@gmail.com>
2023-02-09 23:06:27 +0200

    Complete main nav

--- src/components/NavBar/MainNav.jsx
+++ src/components/NavBar/MainNav.jsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
 import { AppBar, Toolbar, Stack } from "@mui/material";
 import logo from "../../assets/thousand-sunny-logo.png";
-import MainNavButtons from "./MainNavButtons";
+import { lazy, Suspense, useContext } from "react";
+import ThemeContext from "../../store/theme-context";
+import { UserProfileButton } from "./MainButtons";
+import AuthContext from "../../store/Auth-context";
+const MainNavButtons = lazy(() => import("./MainNavButtons"));
+const DrawerButton = lazy(() =>
+  import("./MobileButtons").then((module) => ({ default: module.DrawerButton }))
+);
 
 const MainNav = () => {
+  const { isDesktop } = useContext(ThemeContext);
+  const { user, isAuthenticated } = useContext(AuthContext);
+
   return (
     <AppBar position="sticky">
       <Toolbar>
@@ -18,7 +28,15 @@ const MainNav = () => {
             <img src={logo} alt="website logo" style={{ width: 64 }} />
           </Link>
 
-          <MainNavButtons />
+          <Suspense>{isDesktop && <MainNavButtons />}</Suspense>
+          <Suspense>
+            {!isDesktop && (
+              <Stack direction="row" spacing={2} alignItems="center">
+                {isAuthenticated && <UserProfileButton userImg={user?.image} />}
+                <DrawerButton />
+              </Stack>
+            )}
+          </Suspense>
         </Stack>
       </Toolbar>
     </AppBar>
