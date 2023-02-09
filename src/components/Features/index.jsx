15407e9e7b9203eec56d3535316534bf5584445c
salahcst344 <salahcst344@gmail.com>
2023-02-09 16:57:05 +0200

    Restructure Features

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -1,68 +1,16 @@
-import {
-  Avatar,
-  Box,
-  Container,
-  Divider,
-  Stack,
-  Typography,
-} from "@mui/material";
-import { AttachMoney, Bolt, LocalShipping } from "@mui/icons-material";
+import { memo } from "react";
+import FeatureItem from "./FeatureItem";
+import features from "./features";
+import FeaturesContainer from "./FeaturesContainer";
 
 const Features = () => {
   return (
-    <Box component="section">
-      <Divider sx={{ bgcolor: "primary.light" }} />
-      <Container>
-        <Stack
-          direction={{ md: "row" }}
-          py={3}
-          justifyContent="space-between"
-          gap={2}
-          component="ul"
-        >
-          {featuresContent.map(({ Icon, title, description }) => (
-            <Stack
-              key={title}
-              direction="row"
-              justifyContent={{ md: "center" }}
-              alignItems="center"
-              spacing={2}
-              component="li"
-            >
-              <Avatar sx={{ color: "whitesmoke", bgcolor: "primary.light" }}>
-                <Icon />
-              </Avatar>
-              <Stack>
-                <Typography variant="h6">{title}</Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {description}
-                </Typography>
-              </Stack>
-            </Stack>
-          ))}
-        </Stack>
-      </Container>
-      <Divider sx={{ bgcolor: "primary.light" }} />
-    </Box>
+    <FeaturesContainer>
+      {features.map(({ Icon, title, description }) => (
+        <FeatureItem Icon={Icon} title={title} description={description} />
+      ))}
+    </FeaturesContainer>
   );
 };
 
-const featuresContent = [
-  {
-    Icon: LocalShipping,
-    title: "Free shipping & return",
-    description: "Free Shipping on all orders over $99",
-  },
-  {
-    Icon: AttachMoney,
-    title: "Money back guarantee",
-    description: "100% Money back guarantee",
-  },
-  {
-    Icon: Bolt,
-    title: "Online support 24/7",
-    description: "We always here for you",
-  },
-];
-
-export default Features;
+export default memo(Features);
