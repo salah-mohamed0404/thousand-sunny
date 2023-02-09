ab8c3d152482d60b47ef0bd9bd574dfc8a5cf78a
salahcst344 <salahcst344@gmail.com>
2023-02-09 16:56:13 +0200

    Add features content

---
+++ src/components/Features/features.js
@@ -0,0 +1,21 @@
+import { AttachMoney, Bolt, LocalShipping } from "@mui/icons-material";
+
+const features = [
+  {
+    Icon: LocalShipping,
+    title: "Free shipping & return",
+    description: "Free Shipping on all orders over $99",
+  },
+  {
+    Icon: AttachMoney,
+    title: "Money back guarantee",
+    description: "100% Money back guarantee",
+  },
+  {
+    Icon: Bolt,
+    title: "Online support 24/7",
+    description: "We always here for you",
+  },
+];
+
+export default features;
