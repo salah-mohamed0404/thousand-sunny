4b65a093ff04e1b43223f768238a7bc513f42f30
salahcst344 <salahcst344@gmail.com>
2023-02-09 23:11:57 +0200

    Group all mobile nav buttons

---
+++ src/components/NavBar/MobileButtons/index.js
@@ -0,0 +1,5 @@
+export { default as AuthButton } from "./AuthButton";
+export { default as CartButton } from "./CartButton";
+export { default as HomeButton } from "./HomeButton";
+export { default as WishListButton } from "./WishListButton";
+export { default as DrawerButton } from "./DrawerButton";
