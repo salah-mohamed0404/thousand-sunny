db16b1fff9951c52b6a9eff64bad246dfa191ea6
salahcst344 <salahcst344@gmail.com>
2023-02-09 17:27:08 +0200

    Build cart dialog

---
+++ src/components/NavBar/CartDialog/index.jsx
@@ -0,0 +1,26 @@
+import { Divider } from "@mui/material";
+import { useContext } from "react";
+import CartContext from "../../../store/cart-context";
+import CartDialogActions from "./CartDialogActions";
+import CartDialogContent from "./CartDialogContent";
+import DialogContainer from "./DialogContainer";
+import DialogHeader from "./DialogHeader";
+
+const CartDialog = ({ open, handleClose }) => {
+  const { cartProducts, totalPrice } = useContext(CartContext);
+  return (
+    <DialogContainer open={open} handleClose={handleClose}>
+      <DialogHeader handleClose={handleClose} />
+
+      <Divider />
+
+      <CartDialogContent cartProducts={cartProducts} />
+
+      <Divider />
+
+      <CartDialogActions totalPrice={totalPrice} />
+    </DialogContainer>
+  );
+};
+
+export default CartDialog;
