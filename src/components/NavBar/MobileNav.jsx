fa095c79fb3c6eb5284ba58a0fa90d49dfc42ff0
salahcst344 <salahcst344@gmail.com>
2023-02-09 23:06:57 +0200

    Build mobile nav

--- src/components/NavBar/MobileNav.jsx
+++ src/components/NavBar/MobileNav.jsx
@@ -1,11 +1,62 @@
-import { BottomNavigation, BottomNavigationAction } from "@mui/material";
-import React from "react";
+import { BottomNavigation } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import AuthContext from "../../store/Auth-context";
+import CartDialog from "./CartDialog";
+import {
+  AuthButton,
+  CartButton,
+  HomeButton,
+  WishListButton,
+} from "./MobileButtons";
 
 const MobileNav = () => {
+  const [active, setActive] = useState(0);
+  const [open, setOpen] = useState(false);
+  const { isAuthenticated, logout } = useContext(AuthContext);
+  const { pathname } = useLocation();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = (e) => {
+    setOpen(false);
+  };
+
+  const handleLogOut = () => {
+    logout();
+    setActive(0);
+  };
+
+  useEffect(() => {
+    if (pathname === "/") setActive(0);
+    else if (pathname === "/wishlist") setActive(1);
+    else if (pathname === "/cart") setActive(2);
+    else if (pathname === "/user-profile") setActive(3);
+    else if (pathname === "/login") setActive(4);
+  }, [pathname]);
+
   return (
-    <BottomNavigation>
-      <BottomNavigationAction />
-    </BottomNavigation>
+    <>
+      <BottomNavigation
+        value={active}
+        sx={{
+          width: "100%",
+          position: "fixed",
+          bottom: 0,
+          zIndex: "appBar",
+        }}
+      >
+        <HomeButton />
+        <WishListButton />
+        <CartButton handleOpen={handleOpen} />
+        <AuthButton
+          isAuthenticated={isAuthenticated}
+          handleLogout={handleLogOut}
+        />
+      </BottomNavigation>
+      <CartDialog open={open} handleClose={handleClose} />
+    </>
   );
 };
 
